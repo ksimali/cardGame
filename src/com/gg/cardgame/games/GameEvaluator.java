ceebfad015be218dc752d7e11e9e2a9a8fc95740
ksimali <cassim.ali@outlook.fr>
2024-07-28 23:03:04 -0400

    Setup GameSwingView class which implements GameViewable Interface + Update Main function in Games.java file: Dependency Inversion

--- src/com/gg/cardgame/games/GameEvaluator.java
+++ src/com/gg/cardgame/games/GameEvaluator.java
@@ -3,7 +3,7 @@ package com.gg.cardgame.games;
 import java.util.List;
 
 import com.gg.cardgame.model.Player;
-import com.gg.cardgame.model.PlayingCard;
+//import com.gg.cardgame.model.PlayingCard;
 
 public interface GameEvaluator {
 	public Player evaluateWinner(List<Player>players);
--- src/com/gg/cardgame/games/Games.java
+++ src/com/gg/cardgame/games/Games.java
@@ -2,13 +2,18 @@ package com.gg.cardgame.games;
 
 import com.gg.cardgame.controller.GameController;
 import com.gg.cardgame.model.Deck;
-import com.gg.cardgame.view.CommandLineView;
+//import com.gg.cardgame.view.CommandLineView;
+import com.gg.cardgame.view.GameSwingView;
 
 public class Games {
 
 	public static void main(String[] args) {
-		GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
+		GameSwingView gsv = new GameSwingView();
+		gsv.createAndShowGUI();
+		//GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
 		//GameController gc = new GameController(new Deck(), new View(), new LowCardGameEvaluator());
+		GameController gc = new GameController(new Deck(), gsv, new HighCardGameEvaluator());
+		
 		gc.run();
 
 	}
